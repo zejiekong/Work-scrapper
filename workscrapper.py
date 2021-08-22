e765f4e29506baa12c0e98a159ba700d41a465d8
Ze Jie <66476775+zejiekong@users.noreply.github.com>
2021-08-22 21:16:27 +0800

    Update workscrapper.py

--- workscrapper.py
+++ workscrapper.py
@@ -13,14 +13,14 @@ import json
 from playsound import playsound  
 
 def workscrap():
-    with open('D:\PY Programs\workscrapper\cred.json') as data_file:
+    with open('D:\PY Programs\workscrapper\cred.json') as data_file: #Username and password are stored in json file.
         data = json.load(data_file)
     options = Options()
     options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
     driver.get("https://venus.wis.ntu.edu.sg/PortalServices/ServiceListModule/LaunchService.aspx?type=1&launchSvc=https%3A%2F%2Fvenus%2Ewis%2Entu%2Eedu%2Esg%2FWSS2%2FStudent%2FLogin%2Easpx")
     username_input = driver.find_element_by_xpath("/html/body/div/div/div[2]/table/tbody/tr/td/form/center[1]/table/tbody/tr/td/table/tbody/tr[2]/td[2]/input")
-    username_input.send_keys(data['username'])
+    username_input.send_keys(data['username']) 
     ddelement= Select(driver.find_element_by_xpath('/html/body/div/div/div[2]/table/tbody/tr/td/form/center[1]/table/tbody/tr/td/table/tbody/tr[3]/td[2]/select'))
     ddelement.select_by_value('STUDENT')
     okbutton = driver.find_element_by_xpath("/html/body/div/div/div[2]/table/tbody/tr/td/form/center[1]/table/tbody/tr/td/table/tbody/tr[4]/td/input[1]")
@@ -30,13 +30,11 @@ def workscrap():
     button = driver.find_element_by_xpath("/html/body/div/div/div[2]/table/tbody/tr/td/form/center[1]/table/tbody/tr/td/table/tbody/tr[5]/td/input[1]")
     button.click()
     assignment = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,'//*[@id="ctl00_detail_menuControlStud1_tvCoordt3"]' )))
-    #assignment = driver.find_element_by_xpath('//*[@id="ctl00_detail_menuControlStud1_tvCoordt3"]')
     assignment.click()
     post = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,'//*[@id="ctl00_detail_gvAvailableJob"]/tbody/tr[1]/th[8]/a' )))
-    #post = driver.find_element_by_xpath('//*[@id="ctl00_detail_gvAvailableJob"]/tbody/tr[1]/th[8]/a')
     post.click()
     while True:
-        jobdata = open(r"D:\PY Programs\workscrapper\jobdata.txt","r")
+        jobdata = open(r"D:\PY Programs\workscrapper\jobdata.txt","r") #list of saved job postings as to identify latest ones
         job = jobdata.readlines()
         jobdata.close()
         photo = False
@@ -64,7 +62,7 @@ def workscrap():
                                 break
                         print(driver.window_handles)
                         direct = (r"C:\Users\User\Desktop\%s.png" % i)
-                        driver.get_screenshot_as_file(direct)
+                        driver.get_screenshot_as_file(direct) #Screenshot web page as png and save to desktop.
                         driver.close()
                         driver.switch_to_window(original_window)
                         photo = True
@@ -72,12 +70,15 @@ def workscrap():
                         break
             driver.refresh()
 
-        jobdata = open(r"D:\PY Programs\workscrapper\jobdata.txt","w")
+        jobdata = open(r"D:\PY Programs\workscrapper\jobdata.txt","w") #update job posting list
         for i in joblist:
             jobdata.write(i + "\n")
         jobdata.close()
+        
+        #Following lines can be added to play sound to alert user when there are new jobs.
         # if photo == True:
         #     playsound("D:\PY Programs\workscrapper\jobaudio.mp3")
+        
 
 
     
